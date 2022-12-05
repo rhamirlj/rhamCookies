14650bf5f5d029f37414b5c4b9704f31968e8e4f
Rhamirl Jaafar <rhamirlj5@gmail.com>
2022-12-05 12:58:00 +0800

    Login

--- index.php
+++ index.php
@@ -5,12 +5,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Page</title>
 </head>
 
 <body>
-    <h1>rham modified this page</h1>
-    <h3>Anoher modification</h3>
+    <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <button type="submit">Login</button>
+        <label>
+            <input type="checkbox" checked="checked" name="remember"> Remember me
+        </label>
+    </div>
 </body>
 
 </html>
